919922a0e2afb67c46d058bd09d3f690a27d1e9b
gustavo serrano <gustavo.serrano.valda@gmail.com>
2022-09-16 12:01:42 -0300

    traer/perfil-ok

--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/servicios/portfolio.service';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-encabezado',
@@ -7,11 +8,12 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
   styleUrls: ['./encabezado.component.css']
 })
 export class EncabezadoComponent implements OnInit {
-miporfolio:any;
-  //constructor(private datosporfolio:PortfolioService) { }ng
+  persona: persona = new persona("","");
+
+  constructor(public personaservice: PersonaService){}
 
   ngOnInit(): void {
-    { };
+    this.personaservice.getpersona().subscribe(data => {this.persona =data})
   }
 
 }
